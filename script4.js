775066399d0636f42a9ed0ebb2797ba7bfa8d5f0
Zmulvihill <zmulvihi@yahoo.com>
2023-02-13 13:36:34 -0700

    updated script4 and ran node

--- script4.js
+++ script4.js
@@ -1,5 +1,3 @@
 //Use Arrow Functions to Write Concise Anonymous Functions
-const magic = () => {
-    return new Date();
-  };
-  console.log(magic())
+const magic = () => { return new Date();}
+console.log(magic())

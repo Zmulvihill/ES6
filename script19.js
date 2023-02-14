c271a95e3c88458f601762dd2d4a983c86ee719f
Zmulvihill <zmulvihi@yahoo.com>
2023-02-14 13:54:07 -0700

    added script19 and ran node

---
+++ script19.js
@@ -0,0 +1,22 @@
+//Use getters and setters to Control Access to an Object
+// Only change code below this line
+class Thermostat {
+    constructor(fahrenheit) {
+      this._celsius = (5 / 9) * (fahrenheit - 32);
+    }
+    
+    get temperature() {
+      return this._celsius;
+    }
+    
+    set temperature(celsius) {
+      this._celsius = celsius;
+    }
+  }
+  // Only change code above this line
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
+  console.log(thermos)

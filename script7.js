4b2bc936c85b77d8f13f0b9a12498cd09aa722c7
Zmulvihill <zmulvihi@yahoo.com>
2023-03-13 19:27:26 -0700

    added new script and ran node

--- script7.js
+++ script7.js
@@ -1,4 +1,7 @@
 //Use the Rest Parameter with Function Parameters
 const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);}
-    console.log(sum(1, 2, 3));
+  console.log(sum(0,1,2))
+  console.log(sum(1,2,3,4))
+  console.log(sum(5))
+  console.log(sum())

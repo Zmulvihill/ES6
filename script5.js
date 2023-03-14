c007c5333a18a012ba5e5026a3dcff1739e22f0b
Zmulvihill <zmulvihi@yahoo.com>
2023-03-13 19:10:47 -0700

    added new script and ran node

--- script5.js
+++ script5.js
@@ -1,7 +1,4 @@
 //Write Arrow Functions with Parameters
-const myConcat = (arr1, arr2) => {
-    "use strict";
-    return arr1.concat(arr2);
-  };
-  // test your code
-  console.log(myConcat([1, 2], [3, 4, 5]));
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
